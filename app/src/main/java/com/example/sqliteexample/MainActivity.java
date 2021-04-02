cf2576fcc5a2f9a685223cbba1b2e9978b399789
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2021-04-02 07:20:59 +0200

    Create button and onClickListner + create class EmployeeModelClass

--- app/src/main/java/com/example/sqliteexample/MainActivity.java
+++ app/src/main/java/com/example/sqliteexample/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.sqliteexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static android.view.View.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText_name, editText_email;
+    Button button_add, button_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText_name.findViewById(R.id.edittext_name);
+        editText_email.findViewById(R.id.edittext_email);
+
+        button_add.findViewById(R.id.button_add);
+        button_view.findViewById((R.id.button_view));
+
+        button_add.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             String stringName = editText_name.getText().toString();
+             String stringEmail = editText_email.getText().toString();
+
+             if (stringName.length() <= 0 || stringEmail.length() <= 0) {
+                 Toast.makeText(MainActivity.this, "Enter All Data", Toast.LENGTH_SHORT).show();
+             } else {
+
+
+             }
+            }
+        });
     }
 }
