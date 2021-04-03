99df23d178c84b6a02f3b2257e543acd8467ffe3
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2021-04-03 18:05:44 +0200

    Create new OnClickListener and class ViewEmployeeActivity

--- app/src/main/java/com/example/sqliteexample/MainActivity.java
+++ app/src/main/java/com/example/sqliteexample/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.sqliteexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -46,5 +47,12 @@ public class MainActivity extends AppCompatActivity {
              }
             }
         });
+
+        button_view .setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ViewEmployeeActivity.class);
+                startActivity(intent);
+            }
     }
 }
